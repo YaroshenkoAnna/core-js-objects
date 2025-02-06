a0a101e071d3428d5979f15ddea49472fdd40f9e
Anna <yaroshenkoannette@gmail.com>
2025-02-06 02:32:58 +0100

    feat solution of mergeObjects npm test

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -33,8 +33,21 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  debugger;
+  return objects.length
+    ? Object.fromEntries(
+        Object.entries(
+          objects
+            .map(Object.entries)
+            .flat()
+            .reduce((acc, [key, value]) => {
+              acc[key] = (acc[key] || 0) + value;
+              return acc;
+            }, {})
+        )
+      )
+    : {};
 }
 
 /**
